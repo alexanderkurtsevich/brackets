48b221be2dc479745a55d6e07f2b10ad09f8438a
AlexanderKurtsevich <alexander.kurtsevich@gmail.com>
2020-03-07 00:51:59 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  bracketsConfig = bracketsConfig.map(a => a.join(''));
+  while (bracketsConfig.some(a => str.includes(a))) {
+    for (i = 0; i < bracketsConfig.length; i++) {
+      if (str.includes(bracketsConfig[i])) {
+        str = str.replace(bracketsConfig[i], '');
+      }
+    }
+  }
+  return (str.length == 0) ? true : false
 }
